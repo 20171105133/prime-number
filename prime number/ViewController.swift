dfe35d3f09cbc13c652958c199f9b5afac85585c
20171105133 <164816176@qq.com>
2018-12-13 09:02:07 +0800

    Initial Commit

---
+++ prime number/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prime number
+//
+//  Created by 周世婧 on 2018/12/13.
+//  Copyright © 2018 周世婧. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ prime numberTests/prime_numberTests.swift
@@ -0,0 +1,34 @@
+//
+//  prime_numberTests.swift
+//  prime numberTests
+//
+//  Created by 周世婧 on 2018/12/13.
+//  Copyright © 2018 周世婧. All rights reserved.
+//
+
+import XCTest
+@testable import prime_number
+
+class prime_numberTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
